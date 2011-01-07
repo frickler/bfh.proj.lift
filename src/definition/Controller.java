f8a1175bc982e451e49cf6a9290bfe2f67f055ad
krigu <krigu@cd8b9cc9-3618-466c-b15e-cd40212dc611>
2011-01-07 16:55:34 +0000

    Algoupdate
    git-svn-id: http://sbb.frickler.ch/svn/bfh.proj.lift@76 cd8b9cc9-3618-466c-b15e-cd40212dc611

--- src/definition/Controller.java
+++ src/definition/Controller.java
@@ -7,10 +7,10 @@ import logic.Simulation;
 import exceptions.IllegalActionException;
 
 /**
- * The controller is one of the key-logic-classes. It instances the algorithm and 
- * holds a list with all actions who are not performed yet. The algorithm can query
- * the controller for the action with the highest priority similar to a queue.
- * <br>
+ * The controller is one of the key-logic-classes. It instances the algorithm
+ * and holds a list with all actions who are not performed yet. The algorithm
+ * can query the controller for the action with the highest priority similar to
+ * a queue. <br>
  * The controller is also responsible for starting/stopping a simulation.
  * 
  * 
@@ -30,14 +30,16 @@ public interface Controller {
 	public void performAction(Action action) throws IllegalActionException;
 
 	/**
-	 * Performs the given actions. Usually by giving it to 
-	 * the algorithm
+	 * Performs the given actions. Usually by giving it to the algorithm
 	 * 
-	 * @param action Action to perform
-	 * @throws IllegalActionException Gets called if an action is invalid (invalid levels p.E.)
+	 * @param action
+	 *            Action to perform
+	 * @throws IllegalActionException
+	 *             Gets called if an action is invalid (invalid levels p.E.)
 	 */
-	public void performActions(List<Action> action) throws IllegalActionException;
-	
+	public void performActions(List<Action> action)
+			throws IllegalActionException;
+
 	/**
 	 * Starts the performing of the actions
 	 */
@@ -49,26 +51,38 @@ public interface Controller {
 	public void stopController();
 
 	/**
-	 * Return the action with the smallest timestamp and removes
-	 * it from the controllers datastructure
+	 * Return the action with the smallest timestamp and removes it from the
+	 * controllers datastructure
 	 */
 	public Action getActionWithHighestPriority();
+	
+	/**
+	 * 
+	 * @param action Action to process
+	 * @return Closes elevator to the actions StartLevel
+	 */
+	public Elevator getClosestFreeElevator(Action action);
 
 	/**
 	 * 
-	 * @param startLevel Startlevel (currentlevel)
-	 * @param endlevel Destinationlevel
-	 * @param maxPerson Maximal capacity of elevator
+	 * @param startLevel
+	 *            Startlevel (currentlevel)
+	 * @param endlevel
+	 *            Destinationlevel
+	 * @param maxPerson
+	 *            Maximal capacity of elevator
 	 * @return returns a list of action which fulfill the given criteria (Moving
 	 *         up/down, amount of people in action should not exceed the param
-	 *         maxPerson. All returned actions are removed from the controllers 
+	 *         maxPerson. All returned actions are removed from the controllers
 	 *         datastructure.
 	 */
 	public List<Action> getActions(int startLevel, int endlevel, int maxPerson);
 
 	/**
 	 * Removes an elevator from the simulation
-	 * @param removeId Id of the elevator
+	 * 
+	 * @param removeId
+	 *            Id of the elevator
 	 * @return Wheter the elevator could be removed or not
 	 */
 	public boolean removeElevator(int removeId);
@@ -81,13 +95,16 @@ public interface Controller {
 
 	/**
 	 * Adds a new elevator to the simulation
-	 * @param e Elevator to add
+	 * 
+	 * @param e
+	 *            Elevator to add
 	 */
 	public void addElevator(Elevator e);
 
 	/**
 	 * 
-	 * @param building Set the building for the simulation
+	 * @param building
+	 *            Set the building for the simulation
 	 */
 	public void setBuilding(Building building);
 
@@ -99,7 +116,9 @@ public interface Controller {
 
 	/**
 	 * Starts a random simulation
-	 * @param amountAction Amount of actions for this random simulation
+	 * 
+	 * @param amountAction
+	 *            Amount of actions for this random simulation
 	 */
 	public void startRandomSimulation(int amountAction);
 
@@ -110,11 +129,16 @@ public interface Controller {
 
 	/**
 	 * Starts a simulation with a XML-File
-	 * @param path Path to the XML-File
-	 * @param simulationSpeed Speed of the simulation
-	 * @param actions List of action to process
+	 * 
+	 * @param path
+	 *            Path to the XML-File
+	 * @param simulationSpeed
+	 *            Speed of the simulation
+	 * @param actions
+	 *            List of action to process
 	 */
-	public void startSimulation(String path, int simulationSpeed, List<Action> actions);
+	public void startSimulation(String path, int simulationSpeed,
+			List<Action> actions);
 
 	/**
 	 * Resets the evaluation
@@ -132,9 +156,11 @@ public interface Controller {
 	public void resetActions();
 
 	/**
-	 * After a action is performed, it can 
-	 * be saved in the controller for statistic reasons
-	 * @param a Processed action
+	 * After a action is performed, it can be saved in the controller for
+	 * statistic reasons
+	 * 
+	 * @param a
+	 *            Processed action
 	 */
 	void addDoneAction(Action a);
 
@@ -157,10 +183,18 @@ public interface Controller {
 	public String getAlgorithmName();
 
 	/**
+	 * Sets the new algorithm. If a invalid name is passed, a default
+	 * implementation will be taken
+	 * 
+	 * @param name
+	 *            Name of the Algorithm
+	 */
+	public void setAlgorithmName(String name);
+
+	/**
 	 * 
 	 * @return Amount of unprocessed actions
 	 */
 	public int getTodoActionsAmount();
-
 
 }
